cc6cfc604283f8083bd6535e6ecc065f61de7b94
felipegaiad <fpgaiad@gmail.com>
2018-01-14 19:48:41 -0200

    Fragment called by MainActivity

--- app/src/main/java/br/com/fpg/chesspuzzles/ui/MainActivity.java
+++ app/src/main/java/br/com/fpg/chesspuzzles/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.fpg.chesspuzzles.ui;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.root_layout, new PuzzleFragment());
+        fragmentTransaction.commit();
     }
 }
